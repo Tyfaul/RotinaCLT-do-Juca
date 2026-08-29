ed291b77ef5b8f683491b6488c37828017b86f30
Tyfaul <jenniferthurowoficial@gmail.com>
2026-08-28 21:16:19 -0300

    ADD

---
+++ src/Main.java
@@ -0,0 +1,89 @@
+//Imports ==============================================================================
+import java.util.concurrent.TimeUnit;
+//throws InterruptedException
+import java.util.Scanner;
+import java.util.Random;
+
+/*
+1. Defina as suas 3 opções
+        String[] opcoes = {"Opção A", "Opção B", "Opção C"};
+
+2. Crie o gerador de números aleatórios
+        Random random = new Random();
+
+3. Sorteie um índice (0, 1 ou 2)
+        int indiceSorteado = random.nextInt(opcoes.length);
+
+4. Imprima a opção escolhida
+        System.out.println("A escolha foi: " + opcoes[indiceSorteado]);
+ */
+
+public class Main { //Variáveis ========================================================
+    private static String Estado = "";
+    private static int hunger = 0;
+    private static int fatigue = 0;
+
+    public static void main(String[] args) { //Loop dos métodos ========================
+        int running = 0;//Faz o switch virar um loop
+        Estado = "Working";
+        String[] WorkingReactions = {"(╬▔皿▔)╯", "( •̀ ω •́ )y", "(┬┬﹏┬┬)", "ಥ_ಥ", "눈_눈"};
+        Random random = new Random();
+
+        while (running < 50) {
+            switch (Estado) {
+                case "Working":
+                    hunger += 2;
+                    fatigue += 5;
+                    int WorkingReactionID = random.nextInt(WorkingReactions.length); // Sorteio de reações durante o trabalho (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
+                    System.out.println("Juca está trabalhando ... " + WorkingReactions[WorkingReactionID]);
+                    System.out.println("Fome: " + hunger);
+                    System.out.println("Cansaço: " + fatigue);
+                    if (fatigue > 50) //Condição para ir dormir
+                    {
+                        System.out.println("Bateu um soninho ... ^o^");
+                        Estado = "Sleeping";
+                    }else if (hunger > 10) { //Condição para ir comer
+                        System.out.println("Bateu uma fome ... ผ(•̀_•́ผ)");
+                        Estado = "Eating";
+                    }
+                    break;
+                case "Eating":
+                    hunger -= 5;
+                    hunger = Math.max(0, hunger);
+                    System.out.println("Juca está comendo ... ( *^-^)b");
+                    System.out.println("Fome: " + hunger);
+                    System.out.println("Cansaço: " + fatigue);
+
+                    if(hunger <= 0){
+                        hunger = 0;
+                        System.out.println("Ufa! Já estou cheio... ( *︾▽︾)");
+                        System.out.println("Hora de ir para o trabalho! o(TヘTo)");
+                        Estado = "Working";
+                    }
+
+                    break;
+                case "Sleeping":
+                    hunger += 1;
+                    fatigue -= 10;
+                    fatigue = Math.max(0, fatigue);
+                    System.out.println("Juca está dormindo ... (∪.∪ )zzz");
+                    System.out.println("Fome: " + hunger);
+                    System.out.println("Cansaço: " + fatigue);
+
+                    if(fatigue <= 0) {
+                        fatigue = 0;
+                        if (hunger <=0)
+                        {
+                            System.out.println("Hora de ir para o trabalho! o(TヘTo)");
+                            Estado ="Working";
+                        }else if (hunger > 10){
+                            System.out.println("Bateu uma fome ... ผ(•̀_•́ผ)");
+                            Estado = "Eating";
+                        }
+                    }
+                    break;
+            }
+            running ++;
+        }
+    }
+}
